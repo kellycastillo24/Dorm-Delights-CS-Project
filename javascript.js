ea1c906496cc2732f1e02e16d3e6d16ef81908b6
mkath4824 <kathmunoz4824@gmail.com>
2024-11-21 22:40:33 -0500

    Javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
 // home page
 let homePage = "Welcome to the DormDelights team!";
 console.log(homePage);
+
+let text = "Explore healthy and easy recipies for college students.";
+console.log(text);
+
+let search = "Search Recipes..";
+console.log(search);
+
+let Explore = "Explore Recipes";
+console.log(Explore);
+
+let categories = ['Breakfast Recipes', 'Lunch Recipes', 'Dinner Recipes', 'Snack Recipes','Recipes Rating'];
+
+for (let i = 1; i < categories.length; i++){
+    console.log("Catagories:", categories[i]);
+}
+
+let footer = "© 2024 Dorm Delights | All Rights Reserved";
+console.log(footer);
