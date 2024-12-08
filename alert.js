a9afc868758b27cf5f3233d448a024fa8515e158
mkath4824 <kathmunoz4824@gmail.com>
2024-12-07 21:18:17 -0500

    added a cook time alert

---
+++ alert.js
@@ -0,0 +1,25 @@
+function showMessage(){
+    alert("This recipe takes 15 minutes to prepare!");
+}
+
+function showMessage2(){
+    alert("This recipe takes 20 minutes to prepare!");
+}
+
+function showMessage3(){
+    alert("This recipe takes 10 minutes to prepare!");
+}
+
+function showMessage4(){
+    alert("This recipe takes 8 minutes to prepare!");
+}
+
+function showMessage5(){
+    alert("This recipe takes 25 minutes to prepare!");
+}
+
+function showMessage6(){
+    alert("This recipe takes 30 minutes to prepare!");
+}
+
+
